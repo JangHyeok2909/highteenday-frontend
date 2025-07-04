7e2a55babf6dd0fb1f497d5ec9f72f75ada1bc5c
GUBBIB <moon.dragon250@gmail.com>
2025-07-05 03:19:02 +0900

    회원가입 테스트 페이지 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WritePostPage from './pages/WritePostPage';
 import PostPage from './pages/PostPage';
 import LoginButton from './components/LoginButton/LoginButton';
 import NotFound from './pages/NotFound';
+import Register from './components/LoginRelated/Register';
 
   function App() {
     return (
@@ -12,6 +13,7 @@ import NotFound from './pages/NotFound';
           <Route path="/" element={<LoginButton />}/>
           <Route path="/post/write" element={<WritePostPage />} />
           <Route path="/post/view" element={<PostPage />} />
+          <Route path="/register" element={<Register />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
