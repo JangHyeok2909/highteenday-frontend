bf3589cb8e572865b02ac1eba780c9b9bd74b5d5
GUBBIB <moon.dragon250@gmail.com>
2025-07-11 05:42:56 +0900

    Register 페이지 api 호출 방식 변경

--- src/components/LoginRelated/Register.jsx
+++ src/components/LoginRelated/Register.jsx
@@ -31,10 +31,10 @@ function Register() {
       try {
         const res = await axios.post(`https://highteenday.duckdns.org/api/user/register`,
           {
-            nickname: nickName,
-            name: name,
-            email: email,
-            provider: provider
+            USR_nickname: nickName,
+            USR_name: name,
+            USR_email: email,
+            USR_provider: provider
           },
           {
             withCredentials: true,
