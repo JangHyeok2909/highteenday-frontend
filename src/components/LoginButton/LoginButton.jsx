8d5e6ef8ea7dbc78ea0cbb894a3f7f03ba8346d1
Xssgh <wkdgusgh19@gmail.com>
2025-07-11 20:11:15 +0900

    로그인 추가

--- src/components/LoginButton/LoginButton.jsx
+++ src/components/LoginButton/LoginButton.jsx
@@ -1,51 +1,69 @@
 import React, { useEffect, useState } from "react";
+import "./LoginButton.css"; 
 
 function LoginButton() {
+  const [jwtStatus, setJwtStatus] = useState("Jwt 없음");
 
-    const [jwtStatus, setJwtStatus] = useState("Jwt 없음");
+  useEffect(() => {
+    fetch("https://highteenday.duckdns.org/api/user", {
+      method: "GET",
+      credentials: "include"
+    })
+      .then(response => {
+        if (response.ok) {
+          setJwtStatus("Jwt 작동 중 (로그인 상태)");
+        } else {
+          setJwtStatus("Jwt 없음 (비로그인 상태)");
+        }
+      })
+      .catch(error => {
+        setJwtStatus("오류 발생");
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("https://highteenday.duckdns.org/api/user", {
-            method: "GET",
-            credentials: "include"
-        })
-        .then(response => {
-            if(response.ok){
-                setJwtStatus("Jwt 작동 중 (로그인 상태)");
-            } else {
-                setJwtStatus("Jwt 없음 (비로그인 상태)");
-            }
-        })
-        .catch(error => {
-            setJwtStatus("오류 발생");
-        });
-    
-    }, []);
+  return (
+    <div>
+      {/* 로고 */}
+      <header className="logo-header">
+        <img
+          src="/images/mainLogo.png"
+          alt="메인 로고"
+          className="logo-img"
+        />
+      </header>
 
+      {/* 로그인 */}
+      <div className="login-wrapper">
+        <h2 className="login-title">회원 로그인</h2>
 
-    return(<div>
-        <p>{jwtStatus}</p>
-
-        <a href="https://highteenday.duckdns.org/oauth2/authorization/kakao">
-            <button>카카오 로그인</button>
-        </a> 
-
-        <br></br>
-
-        ======================================================<br></br>
+        <p className="jwt-status">{jwtStatus}</p>
 
         <a href="https://highteenday.duckdns.org/oauth2/authorization/naver">
-            <button>네이버 로그인</button>
+          <img
+            src="/images/navLogin.png"
+            alt="네이버 로그인"
+            className="login-button"
+          />
         </a>
 
-        <br></br>
-
-        ======================================================<br></br>
+        <a href="https://highteenday.duckdns.org/oauth2/authorization/kakao">
+          <img
+            src="/images/kakaLogo.png"
+            alt="카카오 로그인"
+            className="login-button"
+          />
+        </a>
 
         <a href="https://highteenday.duckdns.org/oauth2/authorization/google">
-            <button>구글 로그인</button>
+          <img
+            src="/images/googLogin.png"
+            alt="구글 로그인"
+            className="login-button"
+          />
         </a>
-    </div>);
+      </div>
+    </div>
+  );
 }
 
 export default LoginButton;
