dc3c8a7dd2814dd9d87d961bed0eef8c0cfdf657
dunk1111 <kimyeeun9383@naver.com>
2025-08-02 02:01:27 +0900

    전체 게시판 보기 수정

--- src/components/BoardPreviewExam.jsx
+++ src/components/BoardPreviewExam.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./BoardOverview.css";
+import "./BoardPreview.css";
 
 export default function BoardPreviewExam() {
   const [posts, setPosts] = useState([]);
--- src/components/BoardPreviewFree.jsx
+++ src/components/BoardPreviewFree.jsx
@@ -2,7 +2,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./BoardOverview.css";
+import "./BoardPreview.css";
 
 export default function BoardPreviewFree() {
   const [posts, setPosts] = useState([]);
