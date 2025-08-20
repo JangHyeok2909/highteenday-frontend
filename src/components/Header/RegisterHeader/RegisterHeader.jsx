ba14421319c7deb47c71a4df7afd2534de208d1e
GUBBIB <moon.dragon250@gmail.com>
2025-08-21 00:10:57 +0900

    그냥 테스트 했어용

--- src/components/Header/RegisterHeader/RegisterHeader.jsx
+++ src/components/Header/RegisterHeader/RegisterHeader.jsx
@@ -1,11 +1,11 @@
 // src/components/RegisterHeader.jsx
 import React from "react";
-import "./RegisterHeader.css"; 
+import "./RegisterHeader.css";
 
 const RegisterHeader = ({ title }) => {
   return (
     <header className="header">
-      <h1 className="header-title">{title}</h1>
+      <h1 className="header-title">{"test"}</h1>
       <div className="header-divider"></div>
     </header>
   );
