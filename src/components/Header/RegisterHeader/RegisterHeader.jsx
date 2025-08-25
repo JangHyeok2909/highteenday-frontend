c2b41caddb8562036c616678c0da03ea907bf527
GUBBIB <moon.dragon250@gmail.com>
2025-08-25 20:32:01 +0900

    수정 들어간거

--- src/components/Header/RegisterHeader/RegisterHeader.jsx
+++ src/components/Header/RegisterHeader/RegisterHeader.jsx
@@ -1,17 +1,17 @@
-// src/components/RegisterHeader.jsx
-import React from "react";
-import "./RegisterHeader.css";
-import Tile from "react-calendar/dist/Tile";
+// // src/components/RegisterHeader.jsx
+// import React from "react";
+// import "./RegisterHeader.css";
+// import Tile from "react-calendar/dist/Tile";
 
-const RegisterHeader = ({ title }) => {
-  return (
-    <div id="register-header">
-      <header className="header">
-        <h1 className="header-title">{title}</h1>
-        <div className="header-divider"></div>
-      </header>
-    </div>
-  );
-};
+// const RegisterHeader = ({ title }) => {
+//   return (
+//     <div id="register-header">
+//       <header className="header">
+//         <h1 className="header-title">{title}</h1>
+//         <div className="header-divider"></div>
+//       </header>
+//     </div>
+//   );
+// };
 
-export default RegisterHeader;
+// export default RegisterHeader;
